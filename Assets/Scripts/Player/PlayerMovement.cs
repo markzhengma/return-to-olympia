b4e7227b2716de90686ffdb03f9cacf24356ff94
Mark Ma <mazheng1022@hotmail.com>
2018-04-03 18:44:22 +0800

    handle collision with houses, stop movement on collision, compare selections with collisions

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,87 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Direction currentDir;
-	// Vector2 input;
-	// bool isMoving = false;
-	// Vector3 startPos;
-	// Vector3 endPos;
-	// float t;
-	// private Animator anim;
-
-	// public float walkSpeed = 1f;
-
-	// void Start(){
-	// 	anim = GetComponentInChildren<Animator>();
-	// }
-	
-	// // Update is called once per frame
-	// void Update () {
-	// 	if(!isMoving){
-	// 		input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-	// 		if(Mathf.Abs(input.x) > Mathf.Abs(input.y))
-	// 			input.y = 0;
-	// 		else
-	// 			input.x = 0;
-
-	// 		if(input != Vector2.zero){
-	// 			if(input.x < 0){
-	// 				currentDir = Direction.West;
-	// 			}
-	// 			if(input.x > 0){
-	// 				currentDir = Direction.East;
-	// 			}
-	// 			if(input.y < 0){
-	// 				currentDir = Direction.South;
-	// 			}
-	// 			if(input.y > 0){
-	// 				currentDir = Direction.North;
-	// 			}
-
-	// 			switch(currentDir){
-	// 				case Direction.North:
-	// 					anim.Play("NorthAnimation");
-	// 					break;
-	// 				case Direction.East:
-	// 					anim.Play("EastAnimation");
-	// 					break;
-	// 				case Direction.South:
-	// 					anim.Play("SouthAnimation");
-	// 					break;
-	// 				case Direction.West:
-	// 					anim.Play("WestAnimation");
-	// 					break;
-	// 			}
-
-
-	// 			StartCoroutine(Move(transform));
-	// 		}else{
-	// 			anim.Play("Idle");
-	// 		}
-	// 	}
-	// }
-
-	// public IEnumerator Move(Transform entity){
-	// 	isMoving = true;
-	// 	startPos = entity.position;
-	// 	t = 0;
-
-	// 	endPos = new Vector3(startPos.x + System.Math.Sign(input.x), startPos.y + System.Math.Sign(input.y), startPos.z);
-
-	// 	while (t < 1f){
-	// 		t += Time.deltaTime * walkSpeed;
-	// 		entity.position = Vector3.Lerp(startPos, endPos, t);
-	// 		yield return null;
-	// 	}
-	// 	isMoving = false;
-	// 	yield return 0;
-	// }
-
 	public float speed = 2.0f;
 	private Vector3 target;
 	private float diffX;
 	private float diffY;
 	private Animator anim;
+	private string selectName;
+	private string hitName;
 
 	void Start () {
 		target = transform.position;
@@ -105,6 +31,14 @@ public class PlayerMovement : MonoBehaviour {
 				diffY = Mathf.Abs(Mathf.Abs(target.y) - Mathf.Abs(transform.position.y));
 			}else{
 				diffY = Mathf.Abs(target.y) + Mathf.Abs(transform.position.y);
+			}
+			
+			Vector2 origin = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                                          Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+			RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.zero, 0f);
+			if (hit) {
+				hitName = hit.transform.gameObject.name;
+				print(hit.transform.gameObject.name);
 			}
 		}
 		transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
@@ -127,11 +61,12 @@ public class PlayerMovement : MonoBehaviour {
 			anim.Play("Idle");
 		}
 	}
-}
 
-// enum Direction{
-// 	North,
-// 	East,
-// 	South,
-// 	West
-// }
+	void OnCollisionStay2D(Collision2D other){
+		if(other.gameObject.name == hitName){
+			target.x = transform.position.x;
+			target.y = transform.position.y;
+			hitName = "";
+		}
+	}
+}
